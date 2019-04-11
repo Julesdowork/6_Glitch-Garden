a0fea7abad6395617dd52b5f6032e234d7fa5afa
Julian McNeill <julesdowork@gmail.com>
2019-04-10 20:35:36 -0400

    Click To Spawn Defenders

---
+++ Glitch Garden/Assets/Scripts/GameArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameArea : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+
+    void OnMouseDown()
+    {
+        SpawnDefender();
+    }
+
+    void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity);
+    }
+}
