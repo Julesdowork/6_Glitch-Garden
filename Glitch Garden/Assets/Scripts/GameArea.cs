b30a59464a70470ae9aabc8a8ceddff981dc2c47
Julian McNeill <julesdowork@gmail.com>
2019-04-10 20:42:29 -0400

    Spawn At Mouse Position

--- Glitch Garden/Assets/Scripts/GameArea.cs
+++ Glitch Garden/Assets/Scripts/GameArea.cs
@@ -8,11 +8,18 @@ public class GameArea : MonoBehaviour
 
     void OnMouseDown()
     {
-        SpawnDefender();
+        SpawnDefender(GetSquareClicked());
     }
 
-    void SpawnDefender()
+    void SpawnDefender(Vector2 worldPos)
     {
-        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity);
+        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity);
+    }
+
+    Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
     }
 }
