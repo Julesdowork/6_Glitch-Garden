71c46f905038b78de848becc7ad0f7e9e172d69f
Julian McNeill <julesdowork@gmail.com>
2019-04-13 23:13:01 -0400

    PlayerPrefs To Save Settings

---
+++ Glitch Garden/Assets/test.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefsController.SetMasterVolume(0.4f);
+        Debug.Log("Master volume set to: " + PlayerPrefsController.GetMasterVolume());
+    }
+}
